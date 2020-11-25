b97e60946f2324a79e25ef9453a1669269e9ceb6
ltqin <letao.qin@amd.com>
2020-11-25 21:46:17 +0800

    add an empty pass

---
+++ mlir/lib/Dialect/Letao/Transforms/MultAddTransToAdds.cpp
@@ -0,0 +1,34 @@
+//===- ConvertConst.cpp - Quantizes constant ops --------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "PassDetail.h"
+#include "mlir/Dialect/Letao/Passes.h"
+#include "mlir/Dialect/Letao/LetaoOps.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "mlir/IR/Matchers.h"
+#include "mlir/IR/StandardTypes.h"
+
+
+using namespace mlir;
+using namespace mlir::letao;
+
+namespace {
+struct MultAddTransToAdds : public MultAddTransToAddsBase<MultAddTransToAdds> {
+  void runOnFunction() override;
+};
+
+} // end anonymous namespace
+
+
+void MultAddTransToAdds::runOnFunction() {
+  
+}
+
+std::unique_ptr<OperationPass<FuncOp>> mlir::letao::createMultiAddTransPass() {
+  return std::make_unique<MultAddTransToAdds>();
+}

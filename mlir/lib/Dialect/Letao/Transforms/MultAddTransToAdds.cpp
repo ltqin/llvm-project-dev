a3fc2be35f456063720c4a191f7cd5bbf84adfff
qinletao <ltqin@hotmail.com>
2020-11-26 15:22:37 +0800

    add rewrite modify

--- mlir/lib/Dialect/Letao/Transforms/MultAddTransToAdds.cpp
+++ mlir/lib/Dialect/Letao/Transforms/MultAddTransToAdds.cpp
@@ -7,13 +7,12 @@
 //===----------------------------------------------------------------------===//
 
 #include "PassDetail.h"
-#include "mlir/Dialect/Letao/Passes.h"
 #include "mlir/Dialect/Letao/LetaoOps.h"
+#include "mlir/Dialect/Letao/Passes.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/Matchers.h"
 #include "mlir/IR/StandardTypes.h"
 #include "llvm/Support/raw_ostream.h"
-
 
 using namespace mlir;
 using namespace mlir::letao;
@@ -25,32 +24,22 @@ struct MultAddTransToAdds : public MultAddTransToAddsBase<MultAddTransToAdds> {
 
 } // end anonymous namespace
 
-
 void MultAddTransToAdds::runOnFunction() {
   FuncOp func = getFunction();
 
   func.walk([&](letao::MultiAddOp op) {
-      //llvm::errs() << "Hello: ";
-      //llvm::errs().write_escaped(op.getName()) << '\n';
+    // llvm::errs() << "Hello: ";
+    // llvm::errs().write_escaped(op.getName()) << '\n';
     auto loc = op.getLoc();
     auto operands = op.getOperands();
     auto type = op.getOperand(0).getType();
-    
+
     OpBuilder b(op.getOperation());
-    for(unsigned i = 0; i < operands.size()-1;i++)
-    {
-       // auto newOp = b.create<AddIOp>(loc);
-       // op.output().replaceAllUsesWith(newOp);
-      auto iter = b.create<ConstantIndexOp>(loc, i);
-      // load
-      //auto load = b.create<LoadOp>(loc, type, ValueRange{iter});
-      // add
-      Value add;
-      add = b.create<AddIOp>(loc, op.getOperand(i), op.getOperand(1 + i));
-      // store
-     // auto store = b.create<StoreOp>(loc, add, type, ValueRange{iter});
+    Value add = b.create<AddIOp>(loc, op.getOperand(0), op.getOperand(1));
+    for (unsigned i = 2; i < operands.size(); i++) {
+      add = b.create<AddIOp>(loc, add, op.getOperand(i));
     }
-  
+
     op.erase();
   });
 }
